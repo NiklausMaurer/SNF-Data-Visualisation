d4d7826f1f82b410707cf375d0bac3c1bb9ad177
Niklaus Maurer <nick.is.normal@gmail.com>
2018-03-15 17:40:01 +0100

    implements import of remaining relevant tables

--- Data/extract_data.py
+++ Data/extract_data.py
@@ -33,5 +33,27 @@ cur.executemany("INSERT INTO Output (ProjectNumber, Type, Title, Url, Year) VALU
 con.commit()
 
 
+cur = con.cursor()
+cur.execute("CREATE TABLE Publication (PublicationIDSNSF TEXT, ProjectNumber INTEGER, PeerReviewStatus TEXT, TypeOfPublication TEXT, TitleOfPublication TEXT, Authors TEXT, Status TEXT, PublicationYear, ISBN TEXT, DOI TEXT, ImportSource TEXT, OpenAccessStatus TEXT, OpenAccessType TEXT, OpenAccessURL TEXT, BookTitle TEXT, Publisher TEXT, Editors TEXT, JournalTitle TEXT, Volume TEXT, IssueOrNumber TEXT, PageFrom INTEGER, PageTo INTEGER, ProceedingTitle TEXT, ProceedingPlace TEXT, Abstract TEXT);")
+
+with open('Downloaded/P3_PublicationExport.csv', 'r', encoding="utf-8-sig") as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+    to_db = [(row['Publication ID SNSF'], row['Project Number'], row['Peer Review Status'], row['Type of Publication'], row['Title of Publication'], row['Authors'], row['Status'], row['Publication Year'], row['ISBN'], row['DOI'], row['Import Source'], row['Open Access Status'], row['Open Access Type'], row['Open Access URL'], row['Book Title'], row['Publisher'], row['Editors'], row['Journal Title'], row['Volume'], row['Issue / Number'], row['Page from'], row['Page to'], row['Proceeding Title'], row['Proceeding Place'], row['Abstract']) for row in csvreader]
+
+cur.executemany("INSERT INTO Publication (PublicationIDSNSF, ProjectNumber, PeerReviewStatus, TypeOfPublication, TitleOfPublication, Authors, Status, PublicationYear, ISBN, DOI, ImportSource, OpenAccessStatus, OpenAccessType, OpenAccessURL, BookTitle, Publisher, Editors, JournalTitle, Volume, IssueOrNumber, PageFrom, PageTo, ProceedingTitle, ProceedingPlace, Abstract) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
+con.commit()
+
+
+cur = con.cursor()
+cur.execute("CREATE TABLE Collaboration (ProjectNumber INTEGER, GroupOrPerson TEXT, TypesOfCollaboration TEXT, Country TEXT, ProjectStartDate TEXT, ProjectEndDate TEXT);")
+
+with open('Downloaded/P3_CollaborationExport.csv', 'r', encoding="utf-8-sig") as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+    to_db = [(row['Project Number'], row['Group/Person'], row['Types of collaboration'], row['Country'], row['Project Start Date'], row['Project End Date']) for row in csvreader]
+
+cur.executemany("INSERT INTO Collaboration (ProjectNumber, GroupOrPerson, TypesOfCollaboration, Country, ProjectStartDate, ProjectEndDate) VALUES (?, ?, ?, ?, ?, ?);", to_db)
+con.commit()
+
+
 con.close()
 
