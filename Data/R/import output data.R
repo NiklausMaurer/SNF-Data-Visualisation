4a2fb75e30d3a7673c68d1e4177c327d5634bdbc
Niklaus Maurer <nick.is.normal@gmail.com>
2018-03-23 08:53:32 +0100

    adjusted r-script to new export format

--- Data/R/import output data.R
+++ Data/R/import output data.R
@@ -5,11 +5,18 @@ data <- read.csv2("../Generated/Export.csv")
 
 data$Type <- as.factor(data$Type)
 data$Year <- as.numeric(data$Year)
-data$DisciplineNameHierarchy <- as.factor(data$DisciplineNameHierarchy)
-data$DisciplineName <- as.factor(data$DisciplineName)
+data$DisciplineLevel1 <- as.factor(data$DisciplineLevel1)
+data$DisciplineLevel2 <- as.factor(data$DisciplineLevel2)
+data$DisciplineLevel3 <- as.factor(data$DisciplineLevel2)
 data$Institution <- as.factor(data$Institution)
 data$FundingInstrumentHierarchy <- as.factor(data$FundingInstrumentHierarchy)
 data$FundingInstrument <- as.factor(data$FundingInstrument)
 
 
 
+
+
+table <- table(data$Type, data$DisciplineLevel1)
+
+
+plot(table)
