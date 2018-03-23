6d5bbe8e7caa0b12f52bee2220c5efa8d8decae1
Niklaus Maurer <nick.is.normal@gmail.com>
2018-03-23 09:41:46 +0100

    creates table of data in R

--- Data/R/import output data.R
+++ Data/R/import output data.R
@@ -13,10 +13,11 @@ data$FundingInstrumentHierarchy <- as.factor(data$FundingInstrumentHierarchy)
 data$FundingInstrument <- as.factor(data$FundingInstrument)
 
 
+library(ggplot2)     
+
+s <- as.data.frame(table(data$Type, data$DisciplineLevel1))
 
 
 
-table <- table(data$Type, data$DisciplineLevel1)
-
-
-plot(table)
+ggplot(s, aes(x=s$Var1, y=s$Var2))
++ geom_point(aes(size = s$Freq, colour = Price, alpha=.02))
