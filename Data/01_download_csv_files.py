5f7f960ee918a9929868547bdeaaf9ce2e1d1ea5
Niklaus Maurer <nick.is.normal@gmail.com>
2018-04-30 18:38:03 +0200

    adds some responsiveness to the download script

--- Data/01_download_csv_files.py
+++ Data/01_download_csv_files.py
@@ -5,8 +5,12 @@ import os
 if not os.path.exists("Downloaded"):
     os.makedirs("Downloaded")
 
+print('Downloading P3_PublicationExport.csv')
 urllib.request.urlretrieve("http://p3.snf.ch/P3Export/P3_PublicationExport.csv", "Downloaded/P3_PublicationExport.csv")
+print('Downloading P3_GrantOutputDataExport.csv')
 urllib.request.urlretrieve("http://p3.snf.ch/P3Export/P3_GrantOutputDataExport.csv", "Downloaded/P3_GrantOutputDataExport.csv")
+print('Downloading P3_CollaborationExport.csv')
 urllib.request.urlretrieve("http://p3.snf.ch/P3Export/P3_CollaborationExport.csv", "Downloaded/P3_CollaborationExport.csv")
+print('Downloading P3_GrantExport.csv')
 urllib.request.urlretrieve("http://p3.snf.ch/P3Export/P3_GrantExport.csv", "Downloaded/P3_GrantExport.csv")
 
