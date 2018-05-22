ee6ff493e692433454533a14b0a3f4935334d205
Niklaus Maurer <nick.is.normal@gmail.com>
2018-05-22 10:53:27 +0200

    fixes bug in cluster determination when one axis is 'none'

--- App/src/nim.clustermanager.js
+++ App/src/nim.clustermanager.js
@@ -9,8 +9,8 @@ ClusterManager = function() {
     function generateCacheKey(node, properties)
     {
         var cacheKey = node.group + properties[0] + properties[1];
-        if(properties[0] !== 'None') cacheKey += node.data[properties[0]];
-        if(properties[1] !== 'None') cacheKey += node.data[properties[1]];
+        if(properties[0] !== 'none') cacheKey += node.data[properties[0]];
+        if(properties[1] !== 'none') cacheKey += node.data[properties[1]];
 
         return cacheKey;
     }
@@ -23,10 +23,10 @@ ClusterManager = function() {
         var cacheKey = generateCacheKey(node, properties);
         if(cacheKey in clusterCache) return clusterCache[cacheKey];
 
-        if(nodesCache === null) nodesCache = d3.selectAll('circle');
+        if(nodesCache == null) nodesCache = d3.selectAll('circle');
         var nodesInSameCluster = nodesCache.filter(function(d) {return d.group == node.group;})
-        if(xProperty !== 'none') nodesInSameCluster = nodesInSameCluster.filter(n => n.data[properties[0]] === node.data[properties[0]]);
-        if(yProperty !== 'none') nodesInSameCluster = nodesInSameCluster.filter(n => n.data[properties[1]] === node.data[properties[1]]);
+        if(properties[0] !== 'none') nodesInSameCluster = nodesInSameCluster.filter(n => n.data[properties[0]] === node.data[properties[0]]);
+        if(properties[1] !== 'none') nodesInSameCluster = nodesInSameCluster.filter(n => n.data[properties[1]] === node.data[properties[1]]);
 
         clusterCache[cacheKey] = nodesInSameCluster;
 
